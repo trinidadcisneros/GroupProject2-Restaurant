6b1a250b136146c67cb7f69bc1682adef9793373
trinidadcisneros <33505150+trinidadcisneros@users.noreply.github.com>
2019-05-17 12:05:51 -0700

    updates

--- database.sql
+++ database.sql
@@ -6,15 +6,15 @@ DROP TABLE restaurants;
 
 -- Create tables for raw data to be loaded into
 CREATE TABLE IF NOT EXISTS restaurants (
-    id = Column(Integer, primary_key=True),
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
-    lat FLOAT(5),
-    lng FLOAT(5),
+    lat DECIMAL(6,3),
+    lng DECIMAL(6,3),
     cuisine VARCHAR(255),
-    ave_cost FLOAT(2),
-    rating INTEGER,
-    vote INTEGER,
-    price_range INTEGER,
+    ave_cost INT(2),
+    rating DECIMAL(4,2)
+    vote INT(5),
+    price_range INT(2),
     featured_image VARCHAR(255),
     menu VARCHAR(255)
 );
