18290622ad29f48a3b898b11735c0ace64b59b22
trinidadcisneros <33505150+trinidadcisneros@users.noreply.github.com>
2019-05-29 21:17:05 -0700

    "updated app.js for debugging"
    I was trying to resolve the Ajax issues by nesting functions and I was able to resolve the section that connects the drop down menu to the text box area right below. The text box area populates information about a restaurant with the drop down cuisine category.
    Previously, if I would switch selections in the drop down menu, the text box would crash and no longer update, this is no longer an issue. The next problem was introduced when trying to create a bubble plot that would generate a scatter plot for every restaurant within a cuisine category (x axis the rating, an y axis is the average cost).
    3 issues here: 1) it crashes when you a new drop down cuisine option is selection and 2) the text box from before would no longer render new information. 3) the bubble plot does not update when a new selection is made, I used a method that should clear out the html area Plotly.deleteTraces("bubble", 0); that was used in the belly button homework assignment, but it does not work here.
    
    Get a 500 error - it might be possible that it is crashing when there is only 1 restaurant for a given category. would need help sorting out this issue.

--- app.js
+++ app.js
@@ -1,95 +1,147 @@
-
-// Will provide restaurant informaiton in side panel
-function buildMetadata(business_id) {
-  d3.json(`/restaurant/${business_id}`).then((data) => {
-    var panel = d3.select("#sample-metadata");
-    panel.html("");
-    Object.entries(data).forEach(([key, value]) => {
-    panel.append('h6').text(`${key}: ${value}`);
+function createDropDown() {
+    var selector = d3.select("#selDataset");
+    d3.json("/cuisine_categories").then((sampleNames) => {
+        sampleNames.forEach((sample) => {
+          console.log(sample);
+          selector
+            .append("option")
+            .text(sample)
+            .property("value", sample);
+        });
     });
-  });
-
 }
 
-function getBusinessId(cuisine_category) {
-    // Add the metadata for restaurant at init
+function constructMetaData() {
+    d3.json("/cuisine_categories").then(function(data) {
+        var first_cuisine_selection;
+        first_cuisine_selection = data[0];
+        console.log(first_cuisine_selection);
+        initializeMetaData(first_cuisine_selection);
+    });
+}
+
+function initializeMetaData(cuisine_category) {
     d3.json(`/restaurants/${cuisine_category}`).then(function (data) {
-      console.log(data);
       first_restaurant_in_category = data[0]["restaurant_id"];
       console.log(first_restaurant_in_category);
       buildMetadata(first_restaurant_in_category);
     });
 }
 
+function buildMetadata(business_id) {
+    d3.json(`/restaurant/${business_id}`).then((data) => {
+      var panel = d3.select("#sample-metadata");
+      panel.html("");
+      Object.entries(data).forEach(([key, value]) => {
+      panel.append('h6').text(`${key}: ${value}`);
+      });
+    });
+  
+}
 
+function constructChart() {
+  d3.json("/cuisine_categories").then(function(data) {
+      var cuisine_category;
+      cuisine_category = data[0];
+      console.log(cuisine_category);
+      buildChart(cuisine_category);
+  });
+}
 
 function buildChart(cuisine_category) {
 
-  d3.json(`/restaurants/${cuisine_category}`).then((data) => {
-    var ratings = data.restaurant_rating;
-    var ave_cost = data.ave_cost;
-    console.log(data);
+    d3.json(`/restaurants/${cuisine_category}`).then(function (data) {
+      var ratings = [];
+      var ave_cost = [];
+      data.forEach((d) => { 
+        Object.entries(d).forEach(([key, value]) => {
+          if (key === "restaurant_rating") {
+            ratings.push(value);
+          } if (key === "ave_cost") {
+            ave_cost.push(value);
+          } else {
+          }
+        });
+      });
+      console.log(ratings);
+      console.log(ave_cost);
+  
+      var trace1 = {
+        x: ratings,
+        y: ave_cost,
+        mode: 'markers',
+        marker: {
+          size: ave_cost,
+          color: ratings
+        }
+      };
+  
+      var data1 = [trace1];
+      
+      var layout = {
+        title: "Average Cost (for two) versus Rating",
+        xaxis: { title : "Restaurant Rating (1-5)"},
+        yaxis: { title : "Average cost for two ($)"}
+      };
+      
+      Plotly.plot("bubble", data1, layout);
+    });
+}
+
+function updateBubblePlot(cuisine_category) {
+
+  d3.json(`/restaurants/${cuisine_category}`).then(function (data) {
+    var x_ratings = [];
+    var y_ave_cost = [];
+    data.forEach((d) => { 
+      Object.entries(d).forEach(([key, value]) => {
+        if (key === "restaurant_rating") {
+          x_ratings.push(value);
+        } if (key === "ave_cost") {
+          y_ave_cost.push(value);
+        } else {
+        }
+      });
+    });
     console.log(ratings);
     console.log(ave_cost);
 
-    var trace1 = {
-      x: ratings,
-      y: ave_cost,
+    var trace2 = {
+      x: x_ratings,
+      y: y_ave_cost,
       mode: 'markers',
       marker: {
-        size: ave_cost,
-        color: ratings
+        size: y_ave_cost,
+        color: x_ratings
       }
     };
 
-    var data1 = [trace1];
+    var data2 = [trace2];
     
-    Plotly.plot("bubble", data1);
+    var layout = {
+      title: "Average Cost (for two) versus Rating",
+      xaxis: { title : "Restaurant Rating (1-5)"},
+      yaxis: { title : "Average cost for two ($)"}
+    };
+    
+    Plotly.plot("bubble", data2, layout);
   });
 }
-
 
 
 function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
-
-  // Add categories to dropdown menu
-  d3.json("/cuisine_categories").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      console.log(sample);
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-  
-  const first_category = sampleNames[0];
-  var first_restaurant_in_category;
-
-  // Add the metadata for restaurant at init
-  d3.json(`/restaurants/${first_category}`).then(function (data) {
-    first_restaurant_in_category = data[0]["restaurant_id"];
-    console.log(first_restaurant_in_category);
-    buildMetadata(first_restaurant_in_category);
-  });
-  });
+    createDropDown();
+    constructMetaData();
+    constructChart();
 }
 
-function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
-  // Plotly.deleteTraces('bubble', 0);
-  console.log(newSample);
-
-
-  // Plotly.deleteTraces('pie', 0);will have leaflet map
-  // div.html("") for leaflet to clear map
-  // buildCharts(newSample);
-  getBusinessId(newSample);
-  buildCharts(newSample);
-  // buildMetadata(newSample);
-
+function optionChanged(newSelection) {
+    Plotly.deleteTraces("bubble", 0);
+    console.log(newSelection);
+    initializeMetaData(newSelection);
+    updateBubblePlot(newSelection);
 }
 
-// Initialize the dashboard
+
 init();
